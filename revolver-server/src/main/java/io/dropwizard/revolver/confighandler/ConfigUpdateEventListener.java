0551343df64d203782c35cce0fc6a28a1a834980
Jitendra Dhawan <jitendra.dhawan@phonepe.com>
2020-04-24 03:29:53 +0530

    refactor code

--- revolver-server/src/main/java/io/dropwizard/revolver/confighandler/ConfigUpdateEventListener.java
+++ revolver-server/src/main/java/io/dropwizard/revolver/confighandler/ConfigUpdateEventListener.java
@@ -1,43 +1,8 @@
 package io.dropwizard.revolver.confighandler;
 
-import static io.dropwizard.revolver.confighandler.DynamicConfigHandler.DEFAULT_CONFIG_HASH;
-import static io.dropwizard.revolver.util.CommonUtils.computeHash;
+public interface ConfigUpdateEventListener {
 
-import java.util.Date;
-import lombok.extern.slf4j.Slf4j;
+    void configUpdated(ConfigUpdateEvent configUpdateEvent);
 
-@Slf4j
-public abstract class ConfigUpdateEventListener {
-
-    private String configAttribute;
-
-    private ConfigLoadInfo configLoadInfo;
-
-    public ConfigUpdateEventListener(final String configAttribute) {
-        this.configAttribute = configAttribute;
-        this.configLoadInfo = ConfigLoadInfo.builder()
-                .previousConfigHash(DEFAULT_CONFIG_HASH)
-                .previousLoadTime(new Date())
-                .build();
-    }
-
-    void configUpdated(ConfigUpdateEvent configUpdateEvent) {
-        try {
-            String configString = configUpdateEvent.getUpdatedConfig()
-                    .get(configAttribute).toString();
-            String currentConfigHash = computeHash(configString);
-            log.info("Old Config Hash for {} : {} | New Config Hash: {}", configAttribute,
-                    configLoadInfo.getPreviousConfigHash(), currentConfigHash);
-            if (!configLoadInfo.getPreviousConfigHash().equals(currentConfigHash)) {
-                reloadConfig(configString);
-                configLoadInfo.setPreviousConfigHash(currentConfigHash);
-                configLoadInfo.setPreviousLoadTime(new Date());
-            }
-        } catch (Exception e) {
-            log.error("Error updating "+configAttribute+" configuration", e);
-        }
-    }
-
-    protected abstract void reloadConfig(String configString);
 
 }
--- revolver-server/src/main/java/io/dropwizard/revolver/confighandler/RevolverConfigUpdateEventListener.java
+++ revolver-server/src/main/java/io/dropwizard/revolver/confighandler/RevolverConfigUpdateEventListener.java
@@ -1,5 +1,8 @@
 package io.dropwizard.revolver.confighandler;
 
+import static io.dropwizard.revolver.confighandler.DynamicConfigHandler.DEFAULT_CONFIG_HASH;
+import static io.dropwizard.revolver.util.CommonUtils.computeHash;
+
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.dropwizard.revolver.RevolverBundle;
@@ -8,26 +11,51 @@ import io.dropwizard.revolver.core.config.RevolverConfig;
 import io.dropwizard.revolver.core.config.RevolverConfigHolder;
 import io.dropwizard.revolver.core.model.RevolverExecutorType;
 import java.io.IOException;
+import java.util.Date;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-public class RevolverConfigUpdateEventListener extends ConfigUpdateEventListener {
+public class RevolverConfigUpdateEventListener implements ConfigUpdateEventListener {
 
     private ObjectMapper objectMapper;
 
     private RevolverConfigHolder revolverConfigHolder;
 
+    private String configAttribute;
+
+    private ConfigLoadInfo configLoadInfo;
+
     public RevolverConfigUpdateEventListener(final String configAttribute, final ObjectMapper objectMapper,
             RevolverConfigHolder configHolder) {
-        super(configAttribute);
+        this.configAttribute = configAttribute;
+        this.configLoadInfo = ConfigLoadInfo.builder()
+                .previousConfigHash(DEFAULT_CONFIG_HASH)
+                .previousLoadTime(new Date())
+                .build();
         this.objectMapper = objectMapper.copy();
         this.objectMapper.configure(DeserializationFeature.FAIL_ON_IGNORED_PROPERTIES, false);
         this.objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
         this.revolverConfigHolder = configHolder;
     }
 
-    @Override
-    protected void reloadConfig(String configString) {
+    public void configUpdated(ConfigUpdateEvent configUpdateEvent) {
+        try {
+            String configString = configUpdateEvent.getUpdatedConfig()
+                    .get(configAttribute).toString();
+            String currentConfigHash = computeHash(configString);
+            log.info("Old Config Hash for {} : {} | New Config Hash: {}", configAttribute,
+                    configLoadInfo.getPreviousConfigHash(), currentConfigHash);
+            if (!configLoadInfo.getPreviousConfigHash().equals(currentConfigHash)) {
+                reloadConfig(configString);
+                configLoadInfo.setPreviousConfigHash(currentConfigHash);
+                configLoadInfo.setPreviousLoadTime(new Date());
+            }
+        } catch (Exception e) {
+            log.error("Error updating "+configAttribute+" configuration", e);
+        }
+    }
+
+    private void reloadConfig(String configString) {
         try {
             RevolverConfig revolverConfig = objectMapper.readValue(configString, RevolverConfig.class);
             revolverConfigHolder.setConfig(revolverConfig);
